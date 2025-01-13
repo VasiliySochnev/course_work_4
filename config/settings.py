4e9bb678e155c4f9b627d03c55ebfbb44d744063
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2025-01-14 02:08:15 +0300

    add develop in git

--- config/settings.py
+++ config/settings.py
@@ -16,10 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-!w!jq3_xcyus_l6qub3cm5n0%6#5f__ng%x3n+*w_^orfkkt)n"
 
 # SECURITY WARNING: don't run with debug turned on in production!
